338609c934b6f5b732aa9290a0d13fba2c688865
John Doe <you@example.com>
2022-06-22 14:35:57 +0300

    create final test project

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose user language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    print("\nstart browser for test..")
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options,executable_path="E:\\chromedriver\\chromedriver.exe")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
