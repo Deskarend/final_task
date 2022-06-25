edfcd1ea8bf676e1a08f0f2a6cd43a865ecf25ed
John Doe <you@example.com>
2022-06-25 18:46:10 +0300

    Финишная прямая: готовим код к ревью

--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,7 @@ from .pages.basket_page import BasketPage
 
 
 
-'''def test_guest_can_go_to_login_page(browser):
+def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page=MainPage(browser,link)
     page.open()
@@ -15,7 +15,7 @@ def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()'''
+    page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
--- test_product_page.py
+++ test_product_page.py
@@ -8,8 +8,8 @@ from .pages.login_page import LoginPage
 
 
 from .pages.product_page import ProductPage
-
-'''@pytest.mark.parametrize('link', [
+@pytest.mark.need_review
+@pytest.mark.parametrize('link', [
 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -51,13 +51,13 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.solve_quiz_and_get_code()
     page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE)
     
-
+@pytest.mark.need_review
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -71,7 +71,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.test_is_basket_empty()
-    basket_page.test_is_there_text_empty_basket()'''
+    basket_page.test_is_there_text_empty_basket()
 
 
 class TestUserAddToBasketFromProductPage():
@@ -85,13 +85,14 @@ class TestUserAddToBasketFromProductPage():
         self.login_page.register_new_user(email,password)
         self.login_page.should_be_authorized_user()
 
-    def test_guest_cant_see_success_message(self,browser):
+    def test_user_cant_see_success_message(self,browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
         page = ProductPage(browser, link)
         page.open()
         page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE)
 
-    def test_guest_can_add_product_to_basket(self,browser):
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self,browser):
         link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6"
         page=ProductPage(browser,link)
         page.open()
